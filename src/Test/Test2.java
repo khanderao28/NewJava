05d22913af38b3b21d4567d045f91e6ce0080f94
Khanderao <Khanderao286@gmail.com>
2021-09-18 17:06:19 +0530

    KhanderaoLOcal

---
+++ src/Test/Test2.java
@@ -0,0 +1,13 @@
+package Test;
+
+public class Test2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("test2");
+		System.out.println("Hello");
+
+	}
+
+}
